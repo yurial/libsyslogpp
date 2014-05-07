4f5f4bca7ad2ec638d72544e4597c46bb92868bd
Yuri Dyachenko <pub.yurial@gmail.com>
2014-05-07 14:50:11 +0400

    add cmake support

---
+++ INSTALL.cmake
@@ -0,0 +1,16 @@
+Installation Instructions
+*************************
+
+~/libsyslogpp $ mkdir build
+~/libsyslogpp $ cd build
+~/libsyslogpp/build $ cmake ..
+
+On Debian or with RPM run:
+~/libsyslogpp/build $ make package
+
+And install the result packages
+
+On other OS:
+~/libsyslogpp/build $ make
+~/libsyslogpp/build $ sudo make install # `make deinstall' is not supported
+
